459f80f91af3fa540a421c728f34277d3e0d3a3f
PittiglioFrancesco <francescopittiglio79@gmail.com>
2021-07-12 15:50:48 +0200

    add: milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Searchbar/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Searchbar from './components/Searchbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Searchbar
   }
 }
 </script>
@@ -21,8 +23,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+      <h1 class="text-center">Boolzapp</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+div{
+    width: 100%;
+    h1{
+        text-align: center;
+    }
+}
+
+</style>
