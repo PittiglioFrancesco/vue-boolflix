bb25b510e5f3f64e544be3e47b393f37b14f9de1
PittiglioFrancesco <francescopittiglio79@gmail.com>
2021-07-15 09:55:53 +0200

    fine milestone 4

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -1,32 +1,36 @@
 <template>
-  <div class="film">
-      <ul>
-          <li><img :src="'https://image.tmdb.org/t/p/w342'+info.poster_path" alt=""></li>
-          <li><strong>Titolo: </strong> {{info.title || info.name}} </li>
-          <li><strong>Titolo originale: </strong> {{info.original_title}} </li>
-          <li><strong>Lingua: </strong><img :src="'../assets/img/' + info.original_language + '.png'" :alt="info.original_language"></li>
-          <li><strong>Voto:
-            </strong> {{info.vote_average}}
-            <span v-if="info.vote_average == 10">
-                <i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i>
-            </span>
-            <span v-if="(info.vote_average > 8 && info.vote_average < 10)">
-                <i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="gray fas fa-star"></i>
-            </span>
-            <span v-if="(info.vote_average > 6 && info.vote_average < 8)">
-                <i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i>
-            </span>
-            <span v-if="(info.vote_average > 4 && info.vote_average < 6)">
-                <i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i>
-            </span>
-            <span v-if="(info.vote_average > 2 && info.vote_average < 4)">
-                <i class="yellow fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i>
-            </span>
-            <span v-if="(info.vote_average == 0)">
-                <i class="gray fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i>
-            </span>
-           </li>
-      </ul>
+  <div class="film" :style="{backgroundImage:`url(${image})`}">
+
+    <div class="info">
+    <p class="hide"><strong>Titolo: </strong> {{info.title || info.name}}</p>
+    <p class="hide"><strong>Titolo originale: </strong> {{info.original_title}}</p> 
+    <p class="hide"><strong>Lingua: </strong><img :src="'../assets/img/' + info.original_language + '.png'" :alt="info.original_language"></p>
+    <br>
+    <p class="hide">
+        <strong>Voto: </strong>
+        <span v-if="info.vote_average == 10">
+            <i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i>
+        </span>
+        <span v-if="(info.vote_average > 8 && info.vote_average < 10)">
+            <i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="gray fas fa-star"></i>
+        </span>
+        <span v-if="(info.vote_average > 6 && info.vote_average < 8)">
+            <i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i>
+        </span>
+        <span v-if="(info.vote_average > 4 && info.vote_average < 6)">
+            <i class="yellow fas fa-star"></i><i class="yellow fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i>
+        </span>
+        <span v-if="(info.vote_average > 2 && info.vote_average < 4)">
+            <i class="yellow fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i>
+        </span>
+        <span v-if="(info.vote_average == 0)">
+            <i class="gray fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i><i class="gray fas fa-star"></i>
+        </span>
+        {{info.vote_average}}
+    </p>
+    <p class="hide"><strong>Trama: </strong> {{info.overview}}</p> 
+    </div>
+           
   </div>
 </template>
 
@@ -34,16 +38,17 @@
 export default {
 
     name: "Film",
-    props: ["info"]
+    props: ["info"],
+    data(){
+        return{
+            image: 'https://image.tmdb.org/t/p/w342' + this.info.poster_path
+        }
+    }
 
 }
 </script>
 
 <style lang="scss" scoped>
-
-ul {
-    margin: 30px 0;
-}
 
 .gray{
     color: gray;
@@ -52,5 +57,33 @@ ul {
   .yellow{
     color: gold;
   }
+
+  .info{
+    width: 342px;
+    height: 513px;
+    padding: 10px;
+    color: white;
+    overflow-y: auto;
+  }
+
+  .info:hover{
+    background-color: rgba($color: #000000, $alpha: 0.5);
+  }
+
+  .hide{
+      display: none;
+  }
+  .info:hover .hide{
+      display: inline-block;
+  }
+
+  .film{
+      margin: 40px;
+      padding: 0;
+      background-image: url('https://image.tmdb.org/t/p/w342 + info.poster_path');
+      width: 342px;
+      height: 513px;
+  }
+
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="container">
+  <div class="container-fluid">
 
-    <div class="row">
-        <h1 class="text-center my-5">Boolzapp</h1>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><h1>Boolflix</h1></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-    <div class="row">
-        <button>Reset</button>
-        <input type="text" placeholder="Cerca un film o una serie tv" v-model="textSearch" @keyup="$emit('ricerca', textSearch)">
-        <button>Cerca</button>
-    </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Film</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Serie TV</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Prossimamente</a>
+        </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+        <input class="searchbar form-control mr-sm-2" type="search" placeholder="Cerca un film o una serie tv" v-model="textSearch" @keyup="$emit('ricerca', textSearch)">
+        </form>
+  </div>
+    </nav>
 
   </div>
 </template>
@@ -29,15 +43,8 @@ export default {
 
 <style lang="scss" scoped>
 
-.container{
-    .row{
-        input{
-            width: 80%;
-        }
-        button{
-            width: 10%;
-        }
-    }
+.searchbar{
+    width: 250px;
 }
 
 </style>
